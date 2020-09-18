5fff305ed5f2e248c448440f9d8507a36af4c97c
Adam Lipson <alipson98@gmail.com>
2020-09-18 12:49:54 -0400

    add light tracker to bot.ino

--- JuniorDesign2/bot.ino
+++ JuniorDesign2/bot.ino
@@ -4,26 +4,30 @@ const static int right_F = 5;
 const static int right_R = 6;
 const static int left_F = 9;
 const static int left_R = 10;
+bool lightTrack = false;
 
 const static int inf_reciever = 4;
 
 const int AN_SPEED = 100;
+const int LIGHT_THRESHOLD = 775;
 
 void setup(){
     pinMode(5,OUTPUT);
     pinMode(6,OUTPUT);
     pinMode(9,OUTPUT);
     pinMode(10,OUTPUT);
+    pinMode(A0, INPUT);
     Serial.begin(9600);
 }
 
 
 void loop(){
-    if (digitalRead(inf_reciever)) {
-        Drive_pivot_right();
-    } else {
-        Drive_pivot_left();
-    }
+    // if (digitalRead(inf_reciever)) {
+    //     Drive_pivot_right();
+    // } else {
+    //     Drive_pivot_left();
+    // }
+    Follow_Light();
 }
 
 void readthepins(){
@@ -83,4 +87,36 @@ void Drive_pivot_right(){
   digitalWrite(left_R, LOW);
   digitalWrite(right_F, LOW);
   digitalWrite(right_R, LOW);
+}
+
+void Follow_Light() {
+    if (analogRead(A0) < LIGHT_THRESHOLD) {
+      digitalWrite(greenLED, HIGH);
+      lightTrack = true;
+      while(lightTrack) {
+        Drive_forward();
+          if (analogRead(A0) >= LIGHT_THRESHOLD) {
+            for (int i = 0; i < 4; i++) {
+              Drive_pivot_left();
+              delay(500);
+              Drive_stop();
+              if (analogRead(A0) < LIGHT_THRESHOLD) {
+                break;
+              }
+              Drive_pivot_right();
+              delay(1000);
+              if (analogRead(A0) < LIGHT_THRESHOLD) {
+                break;
+              }
+              if (i == 3) {
+                lightTrack = false;
+              }
+            }
+          
+        }
+    }
+    Drive_stop();
+    digitalWrite(greenLED, LOW);
+  }
+  
 }
