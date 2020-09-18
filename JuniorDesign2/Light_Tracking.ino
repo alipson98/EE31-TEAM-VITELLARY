8a3cbd811bdebc8a0484b4a41da576263fa18701
bglilb <49768663+bglilb@users.noreply.github.com>
2020-09-18 12:47:47 -0400

    Add files via upload

--- JuniorDesign2/Light_Tracking.ino
+++ JuniorDesign2/Light_Tracking.ino
@@ -2,6 +2,7 @@ const int right_1 = 5;
 const int right_2 = 6;
 const int left_3 = 9;
 const int left_4 = 10;
+const int greenLED = 12;
 bool lightTrack = false;
 
 #include <drive.h>
@@ -12,6 +13,7 @@ void setup() {
   pinMode(right_2,OUTPUT);
   pinMode(left_3,OUTPUT);
   pinMode(left_4,OUTPUT);
+  pinMode(greenLED, OUTPUT);
   pinMode (A0, INPUT);
   //Serial.begin(9600);
 }
@@ -22,30 +24,32 @@ void loop() {
 
 void Follow_Light() {
     if (analogRead(A0) < 775) {
-    lightTrack = true;
-    while(lightTrack) {
-      Drive_forward();
-        if (analogRead(A0) >= 775) {
-          for (int i = 0; i < 4; i++) {
-            Drive_pivot_left();
-            delay(500);
-            Drive_stop();
-            if (analogRead(A0) < 775) {
-              break;
+      digitalWrite(greenLED, HIGH);
+      lightTrack = true;
+      while(lightTrack) {
+        Drive_forward();
+          if (analogRead(A0) >= 775) {
+            for (int i = 0; i < 4; i++) {
+              Drive_pivot_left();
+              delay(500);
+              Drive_stop();
+              if (analogRead(A0) < 775) {
+                break;
+              }
+              Drive_pivot_right();
+              delay(1000);
+              if (analogRead(A0) < 775) {
+                break;
+              }
+              if (i == 3) {
+                lightTrack = false;
+              }
             }
-            Drive_pivot_right();
-            delay(1000);
-            if (analogRead(A0) < 775) {
-              break;
-            }
-            if (i == 3) {
-              lightTrack = false;
-            }
-          }
           
         }
     }
     Drive_stop();
+    digitalWrite(greenLED, LOW);
   }
   
 }
