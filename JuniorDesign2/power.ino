c6d9c87827b33e7ab97a3c140846eeaece7d37f9
Adam Lipson <alipson98@gmail.com>
2020-09-23 21:19:42 -0400

    power example

--- JuniorDesign2/power.ino
+++ JuniorDesign2/power.ino
@@ -1,10 +1,15 @@
 //thermistor pin will be A1
 
-int thermPin = A1;
+const int thermPin = A1;
 int tempReading = 0;
 int statusLight = 2;
+const int thermThresh = 267; // TODO: test and change this to the correct value
 
 void setup() {
+  Serial.begin(9600);
+  while (!Serial){
+    ;
+  }
   // put your setup code here, to run once:
 pinMode(thermPin, INPUT);
 pinMode(statusLight, OUTPUT);
@@ -15,23 +20,23 @@ SleepNow();
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("hi");
+  while(true);
+  Serial.println(analogRead(thermPin));
 }
 
 
 void SleepNow(){
-  while(tempReading < 540)
+  Serial.println(analogRead(thermPin));
+  while(tempReading < thermThresh)
   {
-    delay(1000);
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
+    Serial.println(analogRead(thermPin));
     digitalWrite(LED_BUILTIN, LOW);
     tempReading = analogRead(thermPin);
     Serial.println(tempReading);
-    if(tempReading >= 540){
+    if(tempReading >= thermThresh){
       digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(statusLight, HIGH);
-      delay(1500);
-      digitalWrite(statusLight, LOW);
+
       break;
     }
   }
